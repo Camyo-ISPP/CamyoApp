0db50768f6ec84015de6c75563ef6be3c4538bde
Ruben Perez <rupg29@gmail.com>
2025-03-16 21:51:19 +0100

    fix: update main page

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,5 +1,5 @@
 import { router } from "expo-router";
-import { Text, View, StyleSheet, TouchableOpacity, StatusBar, TextInput, Platform, Image, ScrollView, ActivityIndicator } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, StatusBar, TextInput, Platform, Image, ScrollView, ActivityIndicator, Dimensions } from "react-native";
 import colors from "frontend/assets/styles/colors";
 import axios from 'axios';
 import React, { useEffect, useState } from "react";
@@ -7,9 +7,10 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import BottomBar from './_components/BottomBar';
-import { MaterialIcons } from "@expo/vector-icons";
+import { FontAwesome5, MaterialIcons } from "@expo/vector-icons";
 import CamyoWebNavBar from "./_components/CamyoNavBar";
-import defaultCompanyLogo from "frontend/assets/images/defaultCompImg.png"
+const defaultCompanyLogo = require("../assets/images/defaultCompImg.png");
+const truckImage = require("../assets/images/camion.png");
 import Titulo from "./_components/Titulo";
 
 const BACKEND_URL = process.env.EXPO_PUBLIC_BACKEND_URL;
@@ -17,12 +18,20 @@ const BACKEND_URL = process.env.EXPO_PUBLIC_BACKEND_URL;
 export default function Index() {
   const [data, setData] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
-  
+  const [error, setError] = useState(null);
+  const [isCompact, setIsCompact] = useState(Dimensions.get("window").width < 1040);
+
+  useEffect(() => {
+    const updateSize = () => {
+      setIsCompact(Dimensions.get("window").width < 1040);
+    };
+    Dimensions.addEventListener("change", updateSize);
+    return () => Dimensions.removeEventListener("change", updateSize);
+  }, []);
+
   useEffect(() => {
     fetchData();
   }, []);
-
-  const [error, setError] = useState(null);
 
   const fetchData = async () => {
     try {
@@ -43,46 +52,81 @@ export default function Index() {
     );
   }
 
+  const CardOferta = ({ item }) => {
+    return (
+      <View key={item.id} style={styles.card}>
+        <Image source={defaultCompanyLogo} style={styles.companyLogo} />
+        <View style={{ width: "45%" }}>
+          <Text style={styles.offerTitle}>{item.titulo}</Text>
+          <View style={{ display: "flex", flexDirection: "row", marginBottom: 8 }}>
+            <Text style={styles.offerDetailsTagLicense}>{item.licencia.replace(/_/g, '+')}</Text>
+            <Text style={styles.offerDetailsTagExperience}>{">"}{item.experiencia} años</Text>
+            <View style={{ display: "flex", alignItems: "center", flexDirection: "row" }}>
+              <Text style={styles.localizacion}>|</Text>
+              <MaterialIcons name="location-on" size={20} color="#696969" />
+              <Text style={styles.localizacion}>{item.localizacion}</Text>
+            </View>
+          </View>
+          <Text style={styles.offerInfo}>{item.notas}</Text>
+        </View>
+        <View>
+          <Text style={styles.offerSueldo}>{item.sueldo}€</Text>
+          <TouchableOpacity style={styles.button} onPress={() => router.replace(`/oferta/${item.id}`)}>
+            <MaterialCommunityIcons name="details" size={15} color="white" style={styles.detailsIcon} />
+            <Text style={styles.buttonText}>Ver Detalles</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <>
       {Platform.OS === 'web' ? (
         <View style={styles.webContainer}>
           <CamyoWebNavBar />
           <ScrollView style={styles.scrollview} showsVerticalScrollIndicator={false} contentContainerStyle={{ scrollbarWidth: "none" }}>
-          <Titulo texto="Lista de Ofertas" marginTop={100} />
-            <View style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-              {data && data.map((item) => (
-                <View key={item.id} style={styles.card}>
-                  <Image source={defaultCompanyLogo} style={styles.companyLogo} />
-                  <View style={{width:"30%"}}>
-                    <Text style={styles.offerTitle}>{item.titulo}</Text>
+          
+          <View style={styles.heroContainer}>
+            <View style={styles.heroBox}>
+              <View style={styles.textContainer}>
+                <Text style={styles.heroText}>Donde los camioneros y las empresas se encuentran.</Text>
+                <TouchableOpacity style={styles.registerButton} onPress={() => router.replace("/login")}>
+                  <Text style={styles.registerButtonText}>Accede</Text>
+                  <Ionicons name="arrow-forward" size={25} color="white" style={styles.arrowIcon} />
+                </TouchableOpacity>
+              </View>
+              <Image source={truckImage} style={styles.truckImage} resizeMode="contain" />
+            </View>
+          </View>
+          
+          <View style={styles.separator} />
 
-                    <View style={{ display: "flex", flexDirection: "row" }}>
-                      <Text style={styles.offerDetailsTagType}>{item.tipoOferta}</Text>
-                      <Text style={styles.offerDetailsTagLicense}>{item.licencia.replace(/_/g, '+')}</Text>
-                      <Text style={styles.offerDetailsTagExperience}>{">"}{item.experiencia} años</Text> 
+          <Titulo texto="Lista de Ofertas"/>
+          <View style={styles.listaContainer}>
+            {/* Columna de Carga */}
+            <View style={styles.columna}>
+              <View style={styles.columnaTituloContainer}>
+                <FontAwesome5 name="route" size={23} color={colors.secondary} />
+                <Text style={styles.columnaTitulo}>Carga</Text>
+              </View>
+              {data.filter(item => item.tipoOferta === "CARGA").map(item => <CardOferta key={item.id} item={item} />)}
+            </View>
 
-                      <View style={{display:"flex",alignItems:"center",flexDirection:"row"}}>
-                        <Text style={styles.localizacion}>|</Text>
-                        <MaterialIcons name="location-on" size={20} color="#696969" />
-                        <Text style={styles.localizacion}>{item.localizacion}</Text>
-                      </View>
-                      
-                    </View>
+            {/* Columna de Trabajo */}
+            <View style={styles.columna}>
+              <View style={styles.columnaTituloContainer}>
+                <MaterialIcons name="work-history" size={23
+                  
+                } color={colors.secondary} />
+                <Text style={styles.columnaTitulo}>Trabajo</Text>
+              </View>
+              {data.filter(item => item.tipoOferta === "TRABAJO").map(item => <CardOferta key={item.id} item={item} />)}
+            </View>
+          </View>
 
-                    <Text style={styles.offerInfo}>{item.notas}</Text>
+          <View style={styles.separator} />
 
-                    <View/>
-                  </View>
-                    <Text style={styles.offerSueldo}>{item.sueldo}€</Text>
-                    <TouchableOpacity style={styles.button} onPress={()=>router.replace(`/oferta/${item.id}`)}>
-                    <MaterialCommunityIcons name="details" size={15} color="white" style={styles.detailsIcon} />
-                    <Text style={styles.buttonText}>Ver Detalles</Text>
-
-                    </TouchableOpacity>
-                </View>
-              ))}
-            </View >
           </ScrollView >
         </View >
       ) : (
@@ -110,85 +154,12 @@ export default function Index() {
 }
 
 const styles = StyleSheet.create({
-  phone: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    alignContent: "center",
-    alignItems: "center",
-    backgroundColor: colors.mediumGray,
-  },
-  searchIcon: {
-    color: colors.primary,
-    marginRight: 10,
-  },
-  searchView: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    width: "100%",
-    marginTop: "5%",
-    paddingRight: 15,
-    paddingLeft: 15,
-    marginBottom: 20,
-  },
-  menuIcon: {
-    marginRight: 10,
-  },
-  barraSuperior: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    backgroundColor: colors.lightGray,
-    flex: 1,
-    borderRadius: 25,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  searchInput: {
-    backgroundColor: "transparent",
-    padding: 10,
-    borderRadius: 50,
-    borderColor: "transparent",
-    marginRight: 3,
-    outlineStyle: "none",
-  },
   webContainer: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: "center",
     alignContent: "center",
     flex: 1,
-  },
-  card: {
-    backgroundColor: colors.white,
-    padding: 20,
-    marginVertical: 10,
-    width: "70%",
-    borderRadius: 10,
-    display: "flex",
-    flexWrap:"wrap",
-    flexDirection: "row",
-    alignContent: "center",
-    alignItems:"center",
-    borderLeftWidth: 4,
-    borderColor: "red", // Cambia a "green" si quieres un borde verde
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
   },
   scrollview: {
     flex: 1,
@@ -200,10 +171,126 @@ const styles = StyleSheet.create({
     bottom: 40,
   },
 
-  scrollviewIndicator: {
+  separator: {
+    width: "80%",
+    height: 2,
+    backgroundColor: colors.lightGray,
+    alignSelf: "center",
+    marginTop: 60,
+    marginBottom: -40,
+  },
+
+  listaContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignSelf: "center",
+    width: "90%",
+    marginTop: 20,
+  },
+  columna: {
+    width: "50%",
+    alignItems: "center",
+  },
+  columnaTituloContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 12,
+  },
+  columnaTitulo: {
+    fontSize: 25,
+    fontWeight: "bold",
+    marginBottom: 10,
+    marginLeft: 8,
+    alignSelf: "center",
+    color: colors.secondary,
+  },
+
+  /* Estilos Hero */
+  heroContainer: {
+    width: "100%",
+    height: 400,
+    marginTop: 100,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  heroBox: {
+    width: 700,
+    height: 300,
+    backgroundColor: "white",
+    borderRadius: 15,
+    padding: 20,
+    marginRight: 60,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    elevation: 6,
+  },
+  textContainer: {
+    flex: 1,
+    zIndex: 2,
+  },
+  heroText: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: colors.primary,
+    marginBottom: 45,
+    marginRight: 225,
+    marginLeft: 10,
+  },
+  registerButton: {
+    flexDirection: "row",
     backgroundColor: colors.primary,
-    width: 3,
-    borderRadius: 1.5,
+    width: 200,
+    paddingVertical: 16,
+    paddingHorizontal: 55,
+    marginBottom: 10,
+    marginLeft: 50,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  registerButtonText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  arrowIcon: {
+    marginLeft: 10,
+  },
+  truckImage: {
+    position: "absolute",
+    marginLeft: "45%",
+    marginTop: "15%",
+    width: 450,
+    height: 300,
+    borderRadius: 10,
+    alignSelf: "center",
+  },
+
+  /* Estilos Ofertas */
+  card: {
+    backgroundColor: colors.white,
+    padding: 25,
+    marginVertical: 10,
+    width: "65%",
+    borderRadius: 12,
+    flexWrap:"wrap",
+    flexDirection: "row",
+    alignContent: "center",
+    alignItems:"center",
+    borderLeftWidth: 5,
+    borderColor: "red",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
   },
   companyLogo: {
     height: 90,
@@ -241,7 +328,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     flexWrap: "wrap",
   },
-  
   offerDetailsTagExperience: {
     fontSize: 9,
     borderColor: colors.primary,
@@ -258,50 +344,23 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     flexWrap: "wrap",
   },
-  offerDetailsTagType: {
-    fontSize: 9,
-    backgroundColor: colors.primary,
-    color: colors.white,
-    borderRadius: 10,
-    paddingTop: 2,
-    textAlign: "center",
-    textAlignVertical: "center",
-    paddingBottom: 2,
-    paddingLeft: 5,
-    paddingRight: 6,
-    marginRight: 3,
-    fontWeight: "700",
-    flexWrap: "wrap",
-  },
   offerInfo: {
     fontSize: 12,
     color: "gray",
     marginTop: 5,
+    paddingRight: 8,
     flexWrap: "wrap",
   },
   offerSueldo:{
     fontSize:25,
     fontWeight:"bold",
-    textAlign:"right",
-    paddingLeft:3,
     color: colors.secondary,
     textAlignVertical:"center",
-    width:"35%",
     alignSelf:"center"
-  
-
-  },
-  empresa: {
-    fontSize: 20,
-    color: '#0b4f6c',
-    marginTop: 0,
-},
+    },
   button:{
     backgroundColor:colors.primary,
     color:colors.white,
-    paddingLeft:5,
-    paddingRight:5,
-    marginLeft: "2%",
     marginTop:4,
     flexDirection:"row",
     flexWrap:"nowrap",
@@ -310,9 +369,6 @@ const styles = StyleSheet.create({
     borderRadius:10,
     alignItems:"center",
     justifyContent:"center"
-
-
-
   },
   buttonText:{
     color:colors.white,
@@ -324,41 +380,63 @@ const styles = StyleSheet.create({
     marginLeft:3,
     marginTop:3,
     marginRight:5,
-
-  },// Estilos para el perfil
-  profileContainer: {
-    alignItems: "center",
-    marginTop: 100,
-  },
-  profileText: {
-    fontSize: 18,
-    color: colors.primary,
-  },
-  profileLink: {
-    fontSize: 16,
-    color: colors.secondary,
-    marginTop: 10,
-    textDecorationLine: "underline",
-  },
-  // Estilos cuando el usuario no está logueado
-  greetingContainer: {
-    alignItems: "center",
-    marginTop: 20,
-  },
-  greetingText: {
-    fontSize: 18,
-    color: colors.primary,
-  },
-  loginLink: {
-    fontSize: 16,
-    color: colors.secondary,
-    marginTop: 10,
-    textDecorationLine: "underline",
   },
   localizacion: {
     fontSize: 15,
     color: "#696969",
-},
+  },
 
+  /* Estilos móvil */
+  phone: { //
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.mediumGray,
+  },
+  searchIcon: { //
+    color: colors.primary,
+    marginRight: 10,
+  },
+  searchView: { //
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    marginTop: "5%",
+    paddingRight: 15,
+    paddingLeft: 15,
+    marginBottom: 20,
+  },
+  menuIcon: { //
+    marginRight: 10,
+  },
+  barraSuperior: { //
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: colors.lightGray,
+    flex: 1,
+    borderRadius: 25,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  searchInput: { //
+    backgroundColor: "transparent",
+    padding: 10,
+    borderRadius: 50,
+    borderColor: "transparent",
+    marginRight: 3,
+    outlineStyle: "none",
+  },
 
 });
