59fd990133a2700d3c6e053aea68685eae5f98aa
FranciscoPerezUS <fraperman2@alum.us.es>
2025-03-18 20:10:36 +0100

    feat: add payment subscriptions route

--- backend/src/main/java/com/camyo/backend/pago/PagoController.java
+++ backend/src/main/java/com/camyo/backend/pago/PagoController.java
@@ -1,18 +1,20 @@
 package com.camyo.backend.pago;
 
+import com.camyo.backend.suscripcion.PlanNivel;
 import com.camyo.backend.usuario.Usuario;
 import com.camyo.backend.usuario.UsuarioService;
 import com.stripe.Stripe;
 import com.stripe.exception.StripeException;
 import com.stripe.model.Customer;
 import com.stripe.model.PaymentIntent;
-import com.stripe.model.Product;
 import com.stripe.model.checkout.Session;
 import com.stripe.param.PaymentIntentCreateParams;
 import com.stripe.param.checkout.SessionCreateParams;
 import com.stripe.param.checkout.SessionCreateParams.LineItem.PriceData;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -34,38 +36,70 @@ public class PagoController {
 
     @PostMapping("/integrated")
     String integratedCheckout(@RequestBody Pago pago) throws StripeException {
+
         Stripe.apiKey = STRIPE_API_KEY;
+
         String clientBaseURL = "http://localhost:8081";
 
-        // Start by finding an existing customer record from Stripe or creating a new one if needed
+        // Start by finding an existing customer record from Stripe or creating a new
+        // one if needed
         Usuario cliente = usuarioService.obtenerUsuarioActual();
         Customer clienteStripe = CustomerUtil.findOrCreateCustomer(cliente.getEmail(), cliente.getNombre());
 
-       // Create a PaymentIntent and send its client secret to the client
-        PaymentIntentCreateParams params =
-            PaymentIntentCreateParams.builder()
+        // Create a PaymentIntent and send its client secret to the client
+        PaymentIntentCreateParams params = PaymentIntentCreateParams.builder()
                 .setAmount(1L)
                 .setCurrency("eur")
                 .setCustomer(clienteStripe.getId())
                 .setAutomaticPaymentMethods(
-                    PaymentIntentCreateParams.AutomaticPaymentMethods
-                        .builder()
-                        .setEnabled(true)
-                        .build()
-                )
+                        PaymentIntentCreateParams.AutomaticPaymentMethods
+                                .builder()
+                                .setEnabled(true)
+                                .build())
                 .build();
 
         PaymentIntent paymentIntent = PaymentIntent.create(params);
 
         // Send the client secret from the payment intent to the client
         return paymentIntent.getClientSecret();
-
     }
 
-/* 
-    .setRecurring(
-      PriceCreateParams.Recurring.builder()
-        .setInterval(PriceCreateParams.Recurring.Interval.MONTH)
-        .build()
-    )*/
+    @PostMapping("/subscripcion")
+    String newSubscription(@RequestBody PaymentRequest PaymentRequest) throws StripeException {
+
+        Stripe.apiKey = STRIPE_API_KEY;
+
+        String clientBaseURL = System.getenv().get("CLIENT_BASE_URL");
+
+        Usuario cliente = usuarioService.obtenerUsuarioActual();
+        Customer clienteStripe = CustomerUtil.findOrCreateCustomer(cliente.getEmail(), cliente.getNombre());
+
+        BigDecimal planPrecio = switch (PaymentRequest.getPlanNivel()){
+            case GRATIS -> BigDecimal.valueOf(0.00);
+            case BASICO -> BigDecimal.valueOf(24.99);
+            case PREMIUM -> BigDecimal.valueOf(49.99);
+        };
+
+        SessionCreateParams.Builder paramsBuilder =
+            SessionCreateParams.builder()
+                .setMode(SessionCreateParams.Mode.SUBSCRIPTION)
+                .setCustomer(clienteStripe.getId())
+                .setSuccessUrl(clientBaseURL + "/success?session_id={CHECKOUT_SESSION_ID}")
+                .setCancelUrl(clientBaseURL + "/failure");
+        
+        paramsBuilder.addLineItem(
+            SessionCreateParams.LineItem.builder()
+                .setQuantity(1L)
+                .setPriceData(
+                    PriceData.builder()
+                    .setCurrency("eur")
+                    .setUnitAmountDecimal(planPrecio)
+                    .setRecurring(PriceData.Recurring.builder().setInterval(PriceData.Recurring.Interval.MONTH).build())
+                    .build())
+                .build());
+
+        Session session = Session.create(paramsBuilder.build());
+
+        return session.getUrl();
+    }
 }
