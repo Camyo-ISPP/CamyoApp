98f8c4a56652c224fdaf0b88b82922b742e3cb48
Cargarort3 <cargarort3@alum.us.es>
2025-03-25 20:03:28 +0100

    fix: add my offers to navbar

--- frontend/app/_components/CamyoNavBar.jsx
+++ frontend/app/_components/CamyoNavBar.jsx
@@ -60,10 +60,14 @@ export default function CamyoWebNavBar({ onSearch }) {
                     <View style={styles.dot} />
                   </>
               )}
-              {!(!user || !user.rol) && (
+              {user && user.rol !== "ADMIN" && (
                   <>
                     <TouchableOpacity style={styles.button} onPress={() => router.push('/chat/list')}>
-                          <Text style={styles.buttonText}>Mis Mensajes</Text>
+                      <Text style={styles.buttonText}>Mis Mensajes</Text>
+                    </TouchableOpacity>
+                    <View style={styles.dot} />
+                    <TouchableOpacity style={styles.button} onPress={() => router.push('/misofertas')}>
+                      <Text style={styles.buttonText}>Mis Ofertas</Text>
                     </TouchableOpacity>
                     <View style={styles.dot} />
                   </>
