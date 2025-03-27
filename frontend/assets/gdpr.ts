21b2d8b774b3959fd2ac7a3644e5985df8165962
Isabel <isacancor@alum.us.es>
2025-03-27 21:27:58 +0100

    fix: gdpr remove abusive clauses

--- frontend/assets/gdpr.ts
+++ frontend/assets/gdpr.ts
@@ -1,5 +1,6 @@
 export const terminosYCondiciones = `
 1. Introducción
+Última revisión: 27 de marzo de 2025.
 
 ¡Bienvenido a Camyo! Al utilizar nuestra plataforma (en adelante, el Servicio), aceptas los presentes Términos y Condiciones. Si no estás de acuerdo con estos términos, debes abstenerte de usar el Servicio.
 
@@ -7,53 +8,109 @@ export const terminosYCondiciones = `
 - Camionero: Persona que utiliza el Servicio para publicar su disponibilidad laboral o buscar ofertas de transporte de carga.
 - Contenidos del Usuario: Incluye cualquier tipo de información que el Usuario suba a la plataforma (perfiles, ofertas de trabajo, chats, etc.).
 - Empresa: Persona o entidad que usa el Servicio para publicitar información relacionada con su actividad profesional y conectar con camioneros.
-- Plataforma: Sitios web, aplicaciones y APIs asociadas con Camyo.
-- Usuario: Cualquier persona que utilice el Servicio, ya sea camionero, empresa o usuario anónimo.
+- Finalidad: Objetivo fundamental del Servicio, que es la conexión entre profesionales del transporte de mercancías, ya sea transportistas (camioneros) o empresas, mediante la creación y publicación de ofertas de trabajo o de transporte de cargas y perfiles con información detallada de camioneros o empresas y los chats entre camioneros y empresas.
+- Plataforma: Sitios web, aplicaciones y APIs de Camyo.
+- Usuario: Persona que hace uso del Servicio, ya sea camionero, empresa o usuario anónimo.
+- Usuario Anónimo: Usuario que no ha creado una cuenta en la Plataforma.
 
 3. Descripción del Servicio
-Camyo es una plataforma digital que conecta a profesionales del transporte de mercancías en España. Permite a las empresas y camioneros encontrar oportunidades de trabajo de manera eficiente y rápida. Los usuarios anónimos pueden explorar ofertas, mientras que los camioneros y empresas pueden interactuar entre sí.
+Camyo es una plataforma que digitaliza la búsqueda de empleo en el sector de transporte de mercancías en España, permitiendo a profesionales y empresas encontrar oportunidades de forma rápida y eficiente. Permite a las empresas publicar ofertas y a los transportistas, tanto autónomos como empleados, encontrar trabajo de manera rápida y sencilla, facilitando una conexión directa entre oferta y demanda.
+
+Servicios ofrecidos:
+
+- Usuarios Anónimos:
+  - Explorar ofertas de trabajo.
+  - Explorar ofertas de transporte de carga.
+  - Filtrar ofertas de trabajo por palabras clave en su título, experiencia mínima y salario mínimo.
+  - Filtrar ofertas de transporte de carga por sitio de origen o destino, peso mínimo y peso máximo.
+  - Acceder a los detalles de una oferta.
+  - Explorar un listado de las empresas registradas.
+  - Acceder a los detalles de una empresa.
+
+- Camioneros: Todos los servicios ofrecidos a usuarios anónimos, además de:
+  - Crear y editar un perfil detallado de camionero.
+  - Solicitar ofertas de trabajo.
+  - Solicitar ofertas de transporte de carga.
+  - Contactar con empresas mediante un chat.
+  - Escribir reseñas de empresas.
+  - Consultar los detalles de sus ofertas solicitadas.
+
+- Empresas: Todos los servicios ofrecidos a usuarios anónimos, además de:
+  - Crear y editar un perfil detallado de empresa.
+  - Publicar ofertas de trabajo.
+  - Publicar ofertas de transporte de carga.
+  - Contactar con camioneros mediante un chat.
+  - Consultar los detalles de sus ofertas publicadas.
+  - Consultar detalles los camioneros que han solicitado sus ofertas.
+  - Asignar o rechazar ofertas a camioneros.
+
+Camyo también ofrece mejoras a los servicios mediante la suscripción a planes para empresas o pagos únicos.
+
+Planes de suscripción para Empresas:
+
+- Gratis: Solo pueden publicar una oferta con datos incompletos.
+- Básico: Permite publicar hasta tres ofertas de empleo con datos completos.
+- Premium: Permite publicar un número ilimitado de ofertas con datos completos.
+
+Además, las empresas pueden patrocinar un número de ofertas dependiente de su plan de suscripción para destacar en los listados.
+
+- Gratis: Solo pueden patrocinar una oferta a la vez.
+- Básico: Pueden patrocinar dos ofertas a la vez.
+- Premium: Pueden patrocinar tantas ofertas como deseen.
+
+Publicidad: La plataforma mostrará anuncios a los usuarios para generar ingresos adicionales. Tanto empresas como camioneros pueden pagar para eliminar los anuncios.
 
 4. Uso del Servicio
-El Usuario se compromete a usar el Servicio de manera legal y responsable. Está prohibido:
-- Usar el Servicio de manera fraudulenta o engañosa.
-- Publicar contenido ofensivo, racista, xenófobo, homófobo, pornográfico, o que viole derechos humanos.
-- Crear múltiples cuentas para evitar el pago de suscripciones o manipular el sistema.
-- Difundir información falsa o ilegal.
+El Usuario acepta usar el Servicio con fines lícitos y de conformidad con estos Términos y Condiciones, la ley y el orden público. Fundamentalmente, no debe intentar dañar el Servicio, utilizarlo para dañar a terceros o a sí mismo, ni encontrar lagunas en los Términos y Condiciones que permitan que se lleven a cabo las acciones anteriores.
+
+Además, al usar el Servicio, el Usuario se compromete a:
+- Usar Camyo correctamente y legítimamente.
+- No crear múltiples cuentas de Camionero para solicitar una misma oferta de una empresa o crear múltiples cuentas de Empresa para publicitar la misma oferta de trabajo con el fin de evitar el pago de una suscripción.
+- No publicar o difundir Contenidos ilícitos, desleales o que no tengan relación con la finalidad del Servicio.
+- No publicar o difundir Contenidos de carácter racista, xenófobo, homófobo, pornográfico, de apología del terrorismo, o que atenten contra los derechos humanos.
+- No publicar o difundir información falsa o engañosa.
+- No suplantar a otros Usuarios del Servicio.
+- No publicar o difundir cualquier tipo de información sin autorización de los propietarios de los derechos de propiedad intelectual o industrial.
+- No publicar o difundir ofertas de carga para el transporte de mercancía prohibida o ilegal.
+
+El Usuario es responsable de todas las actividades realizadas en su cuenta y se compromete a no compartir, transferir, prestar ni vender su cuenta personal sin nuestra aprobación previa. Cualquier acción realizada en su cuenta se le asignará automáticamente.
 
 5. Cuentas de Usuario
-Al registrarse, el Usuario deberá proporcionar información veraz, como nombre, email, teléfono, y otros datos relacionados con su tipo de cuenta (camionero o empresa). La plataforma no se hace responsable de la veracidad de los datos proporcionados.
+La información que los usuarios deben proporcionar al registrarse varía según el tipo de Usuario. Al registrarse, se solicitará la siguiente información:
+- Camionero: DNI, tipos de licencias de vehículos, tipo de disponibilidad (nacional o internacional), años de experiencia, si tiene el CAP (Certificado de Aptitud Profesional) y si es autónomo.
+- Empresa: página web y NIF.
+
+Siempre asumimos la veracidad y validez de cualquier información personal de los usuarios del Servicio.
 
 6. Pagos
-El Servicio puede ser utilizado de forma gratuita con ciertas limitaciones. Las empresas pueden acceder a suscripciones de pago que ofrecen beneficios adicionales. Las tarifas son no reembolsables, salvo que se indique lo contrario.
-Los pagos se gestionan a través de Stripe Payments Europe, Ltd. Al utilizar el Servicio, el Usuario acepta los términos de Stripe.
+El Servicio se puede utilizar de manera gratuita con ciertas limitaciones. Si el Servicio requiere un pago, el Usuario acepta pagar todas las tarifas aplicables. Ninguna tarifa es reembolsable, salvo lo dispuesto expresamente en estos Términos y Condiciones.
+
+Camyo no presta servicios de pago electrónico ni intermedia en su prestación. Para realizar pagos, deberá formalizar un contrato con la plataforma Stripe Payments Europe, Ltd ("Stripe").
 
 7. Propiedad Intelectual
-El Usuario reconoce que la propiedad intelectual del Servicio, incluyendo software, logotipos, y otros materiales, pertenece a Camyo o a sus licenciantes. El Usuario puede usar la plataforma solo de acuerdo con los términos establecidos, y no tiene derecho a reproducir, distribuir ni modificar los materiales sin consentimiento previo.
+El software, gráficos, ficheros de sonido, animación, vídeo, logotipos y marcas comerciales de Camyo son propiedad intelectual de la empresa o sus licenciantes y están protegidos por derechos de autor, marcas comerciales y otras leyes de propiedad intelectual. El Usuario no adquiere derechos de propiedad sobre estos materiales salvo lo dispuesto específicamente en estos Términos y Condiciones.
+
+El Usuario cede gratuitamente a Camyo los derechos de comunicación pública, reproducción, distribución y transformación de sus Contenidos durante el tiempo que dure la relación.
 
 8. Cancelación
-Camyo se reserva el derecho de suspender el acceso de cualquier Usuario que viole estos Términos y Condiciones. La cancelación de la cuenta implica la pérdida inmediata de acceso al Servicio.
+Tras la cancelación, el Usuario pierde el derecho a usar el Servicio. La cancelación puede deberse al incumplimiento de estos Términos y Condiciones o por cualquier otra razón. Cualquier cargo pendiente adeudado a Camyo en el momento de la cancelación seguirá siendo exigible.
 
-9. Limitación de Responsabilidad
-Camyo no será responsable de daños indirectos o consecuentes derivados del uso del Servicio. La responsabilidad total de Camyo ante el Usuario no superará el importe que este haya pagado por acceder al Servicio en los últimos 12 meses.
-
-10. Ley Aplicable
-Estos Términos y Condiciones se regirán por la legislación española. Cualquier disputa se resolverá en los tribunales de Sevilla, España.
-
-11. Contacto
-Si tienes preguntas o necesitas asistencia, puedes contactarnos a través de:
+9. Contacto
+Si tienes cualquier pregunta sobre estos Términos y Condiciones, necesitas ayuda o deseas contactarnos, puedes hacerlo a través de los siguientes canales:
 - Correo electrónico: camyo.team@gmail.com
 `;
 
 export const politicaDePrivacidad = `
 1. Recogida de Datos
-Recogemos datos como nombre, correo electrónico, teléfono, ubicación y otros datos necesarios para ofrecer el Servicio. Esta información es utilizada exclusivamente para facilitar el contacto entre camioneros y empresas.
+Recogemos datos como nombre, correo electrónico, teléfono, ubicación y otros datos necesarios para ofrecer el Servicio. Esta información se usa exclusivamente para facilitar el contacto entre camioneros y empresas.
 
 2. Uso de Datos
-Utilizamos tus datos para operar y mejorar el Servicio. No compartimos tus datos con terceros sin tu consentimiento, excepto cuando sea necesario para cumplir con la ley o con nuestras obligaciones legales.
+Utilizamos tus datos para operar y mejorar el Servicio. No compartimos tus datos con terceros sin tu consentimiento, excepto cuando sea necesario para cumplir con la ley.
 
 3. Derechos
 Tienes el derecho de acceder, modificar o eliminar tus datos en cualquier momento. Para ello, puedes contactar con nosotros a través de camyo.team@gmail.com.
 
 4. Seguridad
-Implementamos medidas de seguridad para proteger tus datos personales, pero no podemos garantizar su seguridad absoluta debido a la naturaleza de la internet.
+Aplicamos medidas de seguridad para proteger tus datos personales, pero no podemos garantizar su seguridad absoluta debido a la naturaleza de la internet.
 `;
+
