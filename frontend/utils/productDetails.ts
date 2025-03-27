2c3a240fedc5928ae071d072f8fbd971d320b005
Adriana Vento Conesa <adrvencon@alum.us.es>
2025-03-27 19:26:20 +0100

    fix: hotfix update with correct plans

--- frontend/utils/productDetails.ts
+++ frontend/utils/productDetails.ts
@@ -1,20 +1,20 @@
 export const Products = new Map();
 
 Products.set("BASICO", {
-    description: "Permite publicar hasta 10 ofertas de empleo con datos completos.",
+    description: "3 ofertas activas simultáneas + 1 patrocinada. Ideal para pequeñas empresas.",
     image: "/assets/images/icon.png",
     name: "Plan Básico",
     price: 24.99,
     id: "BASICO"
-})
+});
 
 Products.set("PREMIUM", {
-    description: "Permite publicar un número ilimitado de ofertas con datos completos.",
+    description: "Ofertas ilimitadas activas y patrocinadas. Máxima visibilidad y herramientas avanzadas.",
     image: "/assets/images/icon.png",
     name: "Plan Premium",
     price: 49.99,
     id: "PREMIUM"
-})
+});
 
 Products.set("PATROCINAR", {
     description: "Promociona una oferta para destacarla durante un periodo de tiempo.",
