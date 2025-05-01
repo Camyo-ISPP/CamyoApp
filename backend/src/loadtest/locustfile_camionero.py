a9d21f54a1a1a612930faa53be32dc29a3a39964
YaoSpain <junyao@alum.us.es>
2025-05-01 12:08:33 +0200

    fix:fix bug of camionero loadtest

--- backend/src/loadtest/locustfile_camionero.py
+++ backend/src/loadtest/locustfile_camionero.py
@@ -1,22 +1,6 @@
 from locust import HttpUser, task, between
 import random
 
-class CamioneroLoadTest(HttpUser):
-    wait_time = between(1, 3)
-
-    @task(2)
-    def obtener_camionero_por_id(self):
-        camionero_id = random.randint(1, 100)
-        self.client.get(f"/camioneros/{camionero_id}")
-
-    @task(1)
-    def obtener_camionero_por_usuario(self):
-        usuario_id = random.randint(1, 100)
-        self.client.get(f"/camioneros/por_usuario/{usuario_id}")
-
-from locust import HttpUser, task, between
-import random
-
 
 API_URL         = "http://localhost:8080"  # cambia si tu API vive en otro host
 USERNAME        = "emp_piloto1"            # credenciales válidas en tu /auth/signin
